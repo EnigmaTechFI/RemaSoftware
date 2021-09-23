4346ea4ba3580315a52bce98d04e59dc61fb845a
Lorenzo Vettori <lore98.vettori@gmail.com>
2021-09-23 15:15:42 +0200

    Aggiungere i file di progetto.

---
+++ RemaSoftware/Data/ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RemaSoftware.ContextModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace RemaSoftware.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<MyUser> MyUsers { get; set; }
+
+        #region Required
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Seed(modelBuilder);     //Seed
+            base.OnModelCreating(modelBuilder);
+
+        }
+
+        public void Seed(ModelBuilder modelBuilder)
+        {
+            var hasher = new PasswordHasher<MyUser>();
+            var user = new List<MyUser>
+            {
+                new MyUser{UserName="lore_vetto11", NormalizedUserName="LORE_VETTO11", Email="lorenzo.vettori11@gmail.com", NormalizedEmail="LORENZO.VETTORI11@GMAIL.COM", PasswordHash = hasher.HashPassword(null, "Patomilan11!"), Name="Lorenzo", Surname="Vettori", Birthday=DateTime.Parse("30-09-1998"), LockoutEnabled = true, SecurityStamp = Guid.NewGuid().ToString("D")},
+
+            };
+
+            modelBuilder.Entity<MyUser>().HasData(user);
+
+        }
+        #endregion
+
+    }
+}
+
---
+++ RemaSoftware/Startup.cs
@@ -0,0 +1,96 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RemaSoftware.ContextModels;
+using RemaSoftware.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RemaSoftware
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSession(options =>
+            {
+                options.Cookie.IsEssential = true;
+            });
+            services.AddControllersWithViews();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("RemaConnection"))); // per entity framework
+            services.AddIdentity<MyUser, IdentityRole>(opts =>
+            {
+                opts.SignIn.RequireConfirmedEmail = false;
+                opts.SignIn.RequireConfirmedAccount = false;
+                opts.Password.RequireDigit = true;
+                opts.Password.RequireLowercase = true;
+                opts.Password.RequireUppercase = true;
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequiredLength = 8;
+            })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+            //services.AddAuthorization(options =>
+            //{
+            //    options.AddPolicy("RequireAdministratorRole",
+            //        policy => policy.RequireRole("SuperUser", "Admin", "User"));
+            //});
+
+
+            var mvcBuilder = services.AddControllersWithViews();
+#if DEBUG
+            mvcBuilder.AddRazorRuntimeCompilation();
+#endif
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext context)
+        {
+            context.Database.Migrate();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                //app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+                app.UseDeveloperExceptionPage();
+
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseSession();
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
