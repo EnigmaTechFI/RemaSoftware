e1ad7e4a8b9dcb8384837099292ea085299b781e
Lorenzo Vettori <lore98.vettori@gmail.com>
2021-10-15 10:51:52 +0200

    AddOperation

---
+++ RemaSoftware/Controllers/OperationController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RemaSoftware.Models.OperationViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RemaSoftware.Controllers
+{
+    public class OperationController : Controller
+    {
+
+        public OperationController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult AddOperation()
+        {
+            var vm = new AddOperationViewModel();
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult AddOperation(AddOperationViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ RemaSoftware/Models/OperationViewModel/AddOperationViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RemaSoftware.Models.OperationViewModel
+{
+    public class AddOperationViewModel 
+    {
+    }
+}
