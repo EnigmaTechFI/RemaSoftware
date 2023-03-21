840958315eb2c3e2f331f078e7738af0cc8fe58d
lorevetto11 <lorenzo.vettori3@stud.unifi.it>
2023-03-21 16:38:46 +0100

    wip migration;

---
+++ RemaSoftware.Domain/Migrations/20230321153746_RemoveOrderTableV1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RemaSoftware.Domain.Migrations
+{
+    public partial class RemoveOrderTableV1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Order_Operations");
+
+            migrationBuilder.DropTable(
+                name: "Order");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    OrderID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClientID = table.Column<int>(type: "int", nullable: false),
+                    DDT = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    DataIn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataOut = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ID_FattureInCloud = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: true),
+                    Image_URL = table.Column<string>(type: "nvarchar(70)", maxLength: 70, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Note = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Number_Piece = table.Column<int>(type: "int", nullable: false),
+                    Number_Pieces_InStock = table.Column<int>(type: "int", nullable: false),
+                    Price_Uni = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    SKU = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(1)", maxLength: 1, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.OrderID);
+                    table.ForeignKey(
+                        name: "FK_Order_Clients_ClientID",
+                        column: x => x.ClientID,
+                        principalTable: "Clients",
+                        principalColumn: "ClientID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Order_Operations",
+                columns: table => new
+                {
+                    OrderID = table.Column<int>(type: "int", nullable: false),
+                    OperationID = table.Column<int>(type: "int", nullable: false),
+                    Ordering = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order_Operations", x => new { x.OrderID, x.OperationID });
+                    table.ForeignKey(
+                        name: "FK_Order_Operations_Operations_OperationID",
+                        column: x => x.OperationID,
+                        principalTable: "Operations",
+                        principalColumn: "OperationID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Order_Operations_Order_OrderID",
+                        column: x => x.OrderID,
+                        principalTable: "Order",
+                        principalColumn: "OrderID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_ClientID",
+                table: "Order",
+                column: "ClientID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_Operations_OperationID",
+                table: "Order_Operations",
+                column: "OperationID");
+        }
+    }
+}
