ddde8c8e7b6a1949957be4edfb2895e817eac79f
lorevetto11 <lorenzo.vettori3@stud.unifi.it>
2023-03-24 15:40:53 +0100

    [Migration] Remove number pieces in subBatch; Fix price with two decimal;

---
+++ RemaSoftware.Domain/Migrations/20230324143951_RemoveNumberPiecesInSubBatch.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RemaSoftware.Domain.Migrations
+{
+    public partial class RemoveNumberPiecesInSubBatch : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumberPieces",
+                table: "SubBatches");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NumberPieces",
+                table: "SubBatches",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
