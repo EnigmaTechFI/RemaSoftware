aef036e03c44ef46a9517aa95d64afe2a8a3728c
alessio corsi <alessiocorsi74@gmail.com>
2023-05-10 16:27:26 +0200

    Initialize Timekeeper

---
+++ RemaSoftware.Domain/Migrations/20230510125623_Attendance-1.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RemaSoftware.Domain.Migrations
+{
+    public partial class Attendance1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Attendance_Employees_EmployeeID",
+                table: "Attendance");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Attendance",
+                table: "Attendance");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Attendance");
+
+            migrationBuilder.RenameTable(
+                name: "Attendance",
+                newName: "Attendances");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Attendance_EmployeeID",
+                table: "Attendances",
+                newName: "IX_Attendances_EmployeeID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Attendances",
+                table: "Attendances",
+                column: "AttendanceID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Attendances_Employees_EmployeeID",
+                table: "Attendances",
+                column: "EmployeeID",
+                principalTable: "Employees",
+                principalColumn: "EmployeeID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Attendances_Employees_EmployeeID",
+                table: "Attendances");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Attendances",
+                table: "Attendances");
+
+            migrationBuilder.RenameTable(
+                name: "Attendances",
+                newName: "Attendance");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Attendances_EmployeeID",
+                table: "Attendance",
+                newName: "IX_Attendance_EmployeeID");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Attendance",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Attendance",
+                table: "Attendance",
+                column: "AttendanceID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Attendance_Employees_EmployeeID",
+                table: "Attendance",
+                column: "EmployeeID",
+                principalTable: "Employees",
+                principalColumn: "EmployeeID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
