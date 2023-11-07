54af82998ea31a91fbfc9018d470b68bb0849be8
alessio corsi <alessiocorsi74@gmail.com>
2023-11-07 22:17:14 +0100

    Fix automatic machine

--- RemaSoftware.WebApp/Helper/AccountingHelper.cs
+++ RemaSoftware.WebApp/Helper/AccountingHelper.cs
@@ -38,8 +38,8 @@ namespace RemaSoftware.WebApp.Helper
             var op = _subBatchService.GetOperationTimelinesByStatus(OperationTimelineConstant.STATUS_WORKING);
             var productionLiveDtos = new List<ProductionLiveDto>();
             var now = DateTime.Now;
-            bool automaticMachine = _machineService.ConnectMachine().Result.MachineOn;
-            _machineService.ConnectMachine();
+            //bool automaticMachine = _machineService.ConnectMachine().Result.MachineOn;
+            bool automaticMachine = false;
             foreach (var item in op.Where(s => s.MachineId.HasValue).ToList())
             {
                 productionLiveDtos.Add(new ProductionLiveDto()
