b151c7cdb30a95fe9f19e28dd19e2de3a41d037c
alessio199825 <alessio.corsi@enigma-tech.it>
2024-01-25 14:42:21 +0100

    Update solleciti

--- RemaSoftware.WebApp/Controllers/GuestController.cs
+++ RemaSoftware.WebApp/Controllers/GuestController.cs
@@ -125,11 +125,12 @@ public class GuestController : Controller
             {
                 return new JsonResult(new {Result = true, Error = "Errore, nota troppo lunga. Si prega di riprovare."});
             }
-
+ 
             if (note.Length == 0)
             { 
-                note = " ";
+                note = "Nessuna nota";
             }
+            
             var users = await _userManager.GetUsersInRoleAsync(Roles.Admin);
             _guestHelper.SendPrompt(id, users, (await _userManager.GetUserAsync(this.User)).Id, note);
             return new JsonResult(new {Result = true, Error = "DDT sollecitata correttamente."});
