7e79b8d888f91b4ccce045b34e7e91a3c3330002
alessio199825 <alessio.corsi@enigma-tech.it>
2024-03-13 10:10:34 +0100

    Update price table

---
+++ RemaSoftware.Domain/Migrations/20240312090036_CreatePriceTable.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RemaSoftware.Domain.Migrations
+{
+    public partial class CreatePriceTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OperationID1",
+                table: "Operations",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Price",
+                columns: table => new
+                {
+                    PriceID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductID = table.Column<int>(type: "int", nullable: false),
+                    Descrizione = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    PriceVal = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    OperationID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Price", x => x.PriceID);
+                    table.ForeignKey(
+                        name: "FK_Price_Operations_OperationID",
+                        column: x => x.OperationID,
+                        principalTable: "Operations",
+                        principalColumn: "OperationID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Price_Products_ProductID",
+                        column: x => x.ProductID,
+                        principalTable: "Products",
+                        principalColumn: "ProductID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Operations_OperationID1",
+                table: "Operations",
+                column: "OperationID1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Price_OperationID",
+                table: "Price",
+                column: "OperationID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Price_ProductID",
+                table: "Price",
+                column: "ProductID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Operations_Operations_OperationID1",
+                table: "Operations",
+                column: "OperationID1",
+                principalTable: "Operations",
+                principalColumn: "OperationID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Operations_Operations_OperationID1",
+                table: "Operations");
+
+            migrationBuilder.DropTable(
+                name: "Price");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Operations_OperationID1",
+                table: "Operations");
+
+            migrationBuilder.DropColumn(
+                name: "OperationID1",
+                table: "Operations");
+        }
+    }
+}
---
+++ RemaSoftware.Domain/Migrations/20240313090640_UpdatePriceTable.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RemaSoftware.Domain.Migrations
+{
+    public partial class UpdatePriceTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Descrizione",
+                table: "Price");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CodeBC",
+                table: "Price",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationDate",
+                table: "Price",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Price",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Note",
+                table: "Price",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CodeBC",
+                table: "Price");
+
+            migrationBuilder.DropColumn(
+                name: "CreationDate",
+                table: "Price");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Price");
+
+            migrationBuilder.DropColumn(
+                name: "Note",
+                table: "Price");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Descrizione",
+                table: "Price",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
